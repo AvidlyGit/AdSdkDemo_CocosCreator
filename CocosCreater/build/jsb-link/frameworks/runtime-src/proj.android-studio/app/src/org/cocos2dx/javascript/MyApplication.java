aae7926cde24e0fefe6bd34f4608f9d25a73307c
huobanbengkui <wk.guo@avid.ly>
2018-12-03 16:17:58 +0800

    添加CocosCreator项目Demo

---
+++ CocosCreater/build/jsb-link/frameworks/runtime-src/proj.android-studio/app/src/org/cocos2dx/javascript/MyApplication.java
@@ -0,0 +1,17 @@
+package org.cocos2dx.javascript;
+
+import android.app.Application;
+import android.content.Context;
+//import android.support.multidex.MultiDex;
+
+public class MyApplication {
+
+
+}
+
+//Application
+//    @Override
+//    protected void attachBaseContext(Context base) {
+//        super.attachBaseContext(base);
+//        MultiDex.install(this);
+//    }
